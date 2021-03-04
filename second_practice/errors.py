67d979968ed97bd5519b0aeeb285fa532cda908d
Vlad <r26101999v@gmail.com>
2021-03-04 10:16:08 +0300

    Доделал дз

---
+++ second_practice/errors.py
@@ -0,0 +1,25 @@
+# for i in range(0, 2)
+#     print()
+
+for i in range(0, 2):
+    print()
+
+a = 5
+# if(a=5):
+#     print()
+if(a == 5):
+    print()
+
+# print('<' + 3)
+print('<' + str(3))
+
+# print(b)
+b = 3
+print(b)
+
+
+# def sum(a, b):
+# return a + b
+
+def sum(a, b):
+    return a + b
